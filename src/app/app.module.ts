3476d6220de2beccb0cbac54f51168e7b892503c
gauravpathak <gauravpathak.spring@gmail.com>
2021-02-06 22:09:06 -0500

    Json Server API Integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavComponent } from './components/shared/nav/nav.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { CartComponent } from './components/shopping-cart/cart/cart.component';
-import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
-import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
-import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
-import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/shared/header/header.component';
+import {FooterComponent} from './components/shared/footer/footer.component';
+import {NavComponent} from './components/shared/nav/nav.component';
+import {ShoppingCartComponent} from './components/shopping-cart/shopping-cart.component';
+import {CartComponent} from './components/shopping-cart/cart/cart.component';
+import {FiltersComponent} from './components/shopping-cart/filters/filters.component';
+import {ProductListComponent} from './components/shopping-cart/product-list/product-list.component';
+import {CartItemComponent} from './components/shopping-cart/cart/cart-item/cart-item.component';
+import {ProductItemComponent} from './components/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
@@ -26,9 +27,11 @@ import { ProductItemComponent } from './components/shopping-cart/product-list/pr
     ProductItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,23 +1,19 @@
 import {Injectable} from '@angular/core';
 import {Product} from '../models/product';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+const apiUrl = 'http://localhost:3000/products/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  products: Product[] = [
-    new Product(1, 'Sanitzer ABC', 'This is product 1 description', 234),
-    new Product(2, 'Sanitzer DEF', 'This is product 2 description', 231),
-    new Product(3, 'Sanitzer GHI', 'This is product 3 description', 238),
-    new Product(4, 'Sanitzer JKL', 'This is product 4 description', 245),
-    new Product(5, 'Sanitzer MNO', 'This is product 5 description', 229),
-    new Product(6, 'Sanitzer PQR', 'This is product 6 description', 230)
-  ];
 
-  constructor() {
+  constructor(private client: HttpClient) {
   }
 
-  getProducts(): Product[] {
-    return this.products;
+  getProducts(): Observable<Product[]> {
+    return this.client.get<Product[]>(apiUrl);
   }
 }
